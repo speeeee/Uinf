5a861e0acaa0ef69f1b0a78b69b81e6c38310eca
Spencer <snsallay@gmail.com>
2015-03-10 22:03:39 -0400

    fix font and add string->font

--- FontMaker.hs
+++ FontMaker.hs
@@ -1,4 +1,4 @@
-module FontMaker (Character(..), Font(..), renderChar, readFont) where
+module FontMaker (Character(..), Font(..), renderChar, renderText, readFont) where
 
 import qualified Graphics.UI.GLFW as K
 import Graphics.Rendering.OpenGL.Raw
@@ -13,6 +13,17 @@ data Character = Character { key :: Char,
                              char :: [(Bool,(GLfloat,GLfloat))], --5x5
                              diacritic :: [(Bool,(GLfloat,GLfloat))] {-5x3-} } deriving (Show,Eq)
 
+renderText :: String -> [Character] -> (GLfloat,GLfloat) -> GLfloat -> IO ()
+renderText s f (x,y) sz =
+  --let ch = findChr (k-97) f
+  mapM_ (\(n,k) -> case findChr k f of Just ch -> renderChar ch (x+(n+n/10)*sz*5,y) sz
+                                       Nothing -> print "Char does not exist!") (zip [0..] (map ord s))
+
+findChr :: Int -> [Character] -> Maybe Character
+findChr n f =
+  let c = chr n in
+  find (\k -> key k == c) f
+
 renderChar :: Character -> (GLfloat,GLfloat) -> GLfloat -> IO ()
 renderChar charac pt sz = do
   renderList sz pt (toCoords (char charac))
@@ -26,11 +37,11 @@ renderList sz pt lst = mapM_ (\(x,y) -> mksquare sz pt (x,y)) lst
 
 mksquare :: GLfloat -> (GLfloat,GLfloat) -> (GLfloat,GLfloat) -> IO ()
 mksquare sz (xt,yt) (xi,yi) = do
-  let (x,y) = (xt+sz*xi,yt+sz*yi)
+  let (x,y) = (xt+sz*xi,yt+(5*sz-sz*yi))
   glVertex3f x y 0
   glVertex3f (x+sz) y 0
-  glVertex3f (x+sz) (y+sz) 0
-  glVertex3f x (y+sz) 0
+  glVertex3f (x+sz) (y-sz) 0
+  glVertex3f x (y-sz) 0
 
 readFont :: FilePath -> IO [Character]
 readFont f = do
